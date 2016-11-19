6d583cca445423ac6a5eb0a8db3c58b8d9827ee3
DAB0mB <emanor6@gmail.com>
2016-11-19 12:49:16 -0200

    Step 3.7: Create 'CalculateSync' method stub

--- src/distance.cc
+++ src/distance.cc
@@ -1,8 +1,12 @@
 #include <nan.h>
 #include <v8.h>
 
-NAN_MODULE_INIT(Init) {
+NAN_METHOD(CalculateSync) {
 
+}
+
+NAN_MODULE_INIT(Init) {
+  NAN_EXPORT(target, CalculateSync);
 }
 
 NODE_MODULE(distance, Init)
