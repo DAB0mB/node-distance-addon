d8f5b1fd8d4823bca227a89beea8476b7d08333d
DAB0mB <emanor6@gmail.com>
2018-08-29 13:09:24 +0800

    master@1.0.0: Initial release - v1

---
+++ .tortilla/manuals/templates/step3.tmpl
@@ -0,0 +1,3 @@
+You've just learned the very basics of how to use C++ within NodeJS. There's a lot more to learn when it comes to building an add-on, and I'm not just talking about learning v8 and Nan's API. Think about the possibilities, the C++ community have been developed for years and there are so much great libraries out there that are not necessarily relevant to NodeJS due to its efficiency, like [Boost](http://www.boost.org/), [OpenCV](http://opencv.org/), [CGAL](http://www.cgal.org/) and many more.
+
+This tutorial didn't focus about algorithmics obviously, it's a good starting point though, because from here I can take it to wherever I want, but I won't reveal my future plans in due course. Hopefully you liked it, stay tuned for upcoming tutorials about this topic.
---
+++ src/distance.cc
@@ -0,0 +1,98 @@
+#include <cstdlib>
+#include <cmath>
+#include <nan.h>
+#include <v8.h>
+
+using Nan::AsyncQueueWorker;
+using Nan::AsyncWorker;
+using Nan::Callback;
+using Nan::HandleScope;
+using Nan::New;
+using Nan::Null;
+using Nan::To;
+using std::pow;
+using std::sqrt;
+using v8::Function;
+using v8::Local;
+using v8::Number;
+using v8::Object;
+using v8::String;
+using v8::Value;
+
+struct Point {
+  double x;
+  double y;
+};
+
+double CalculateDistance(Point* pointA, Point* pointB) {
+  return sqrt(pow(pointA->x - pointB->x, 2) + pow(pointA->y - pointB->y, 2));
+}
+
+class DistanceWorker : public AsyncWorker {
+ private:
+  double distance;
+  Point* pointA;
+  Point* pointB;
+
+ public:
+  DistanceWorker(Callback* callback, Point* pointA, Point* pointB) :
+    AsyncWorker(callback), pointA(pointA), pointB(pointB) {}
+
+  ~DistanceWorker() {
+    delete pointA;
+    delete pointB;
+  }
+
+  void Execute () {
+    distance = CalculateDistance(pointA, pointB);
+  }
+
+  void HandleOKCallback () {
+    HandleScope scope;
+
+    Local<Value> argv[] = {
+      Null(),
+      New<Number>(distance)
+    };
+
+    callback->Call(2, argv);
+  }
+};
+
+NAN_METHOD(CalculateSync) {
+  Local<Object> js_pointA = To<Object>(info[0]).ToLocalChecked();
+  Local<Object> js_pointB = To<Object>(info[1]).ToLocalChecked();
+
+  Point* pointA = new Point();
+  pointA->x = To<double>(js_pointA->Get(New<String>("x").ToLocalChecked())).FromJust();
+  pointA->y = To<double>(js_pointA->Get(New<String>("y").ToLocalChecked())).FromJust();
+
+  Point* pointB = new Point();
+  pointB->x = To<double>(js_pointB->Get(New<String>("x").ToLocalChecked())).FromJust();
+  pointB->y = To<double>(js_pointB->Get(New<String>("y").ToLocalChecked())).FromJust();
+
+  info.GetReturnValue().Set(CalculateDistance(pointA, pointB));
+}
+
+NAN_METHOD(CalculateAsync) {
+  Local<Object> js_pointA = To<Object>(info[0]).ToLocalChecked();
+  Local<Object> js_pointB = To<Object>(info[1]).ToLocalChecked();
+  Callback* callback = new Callback(info[2].As<Function>());
+
+  Point* pointA = new Point();
+  pointA->x = To<double>(js_pointA->Get(New<String>("x").ToLocalChecked())).FromJust();
+  pointA->y = To<double>(js_pointA->Get(New<String>("y").ToLocalChecked())).FromJust();
+
+  Point* pointB = new Point();
+  pointB->x = To<double>(js_pointB->Get(New<String>("x").ToLocalChecked())).FromJust();
+  pointB->y = To<double>(js_pointB->Get(New<String>("y").ToLocalChecked())).FromJust();
+
+  AsyncQueueWorker(new DistanceWorker(callback, pointA, pointB));
+}
+
+NAN_MODULE_INIT(Init) {
+  NAN_EXPORT(target, CalculateSync);
+  NAN_EXPORT(target, CalculateAsync);
+}
+
+NODE_MODULE(distance, Init)
